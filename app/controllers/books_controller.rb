189c98392d2cd63c7b04f8a3c30eb933e6f8b85f
miku0107 <138674231+miku0107@users.noreply.github.com>
2023-08-05 10:20:11 +0000

    fin?

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,8 +5,15 @@ class BooksController < ApplicationController
   
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    
+    if @book.save
+       flash[:notice] ="Book was successfully created."
+       redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+    
   end  
     
   def index
@@ -24,8 +31,16 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    
+    if book.update(book_params)
+       flash[:notice] ="Book was successfully updated."
+       redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      @book.update(book_params)
+      render :edit
+    end
+    
   end
   
   def destroy
